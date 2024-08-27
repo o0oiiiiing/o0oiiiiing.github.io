8cb580bc1a41c40b26b06d9bcc72d22764573503
박다현 <oing_0ing@naver.com>
2024-08-28 06:13:19 +0900

    240828 - header의 nav 작성 중

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -6,6 +6,14 @@ export default function Header() {
         <>
             <header className='header'>
                 <h1 className='header__logo'>Dahyeon Park</h1>
+                <nav className='nav'>
+                    <ul className='nav__list'>
+                        <li className='nav__item'>홈</li>
+                        <li className='nav__item'>메뉴1</li>
+                        <li className='nav__item'>메뉴2</li>
+                        <li className='nav__item'>메뉴3</li>
+                    </ul>
+                </nav>
             </header>
         </>
     )
