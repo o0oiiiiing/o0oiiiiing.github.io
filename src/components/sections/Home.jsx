ef76c1d6340014370b668e721a05b022dff7b3e1
박다현 <oing_0ing@naver.com>
2024-09-24 04:30:26 +0900

    240924 - 스크롤에 따른 배경 효과 구현

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../styles/sections/home.css';
 
 export default function Home() {
+    const [scrollY, setScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <>
             <main className='section__home'>
-                <div className='background background__gradient' />
+                <div className='background background__gradient'
+                    style={{
+                        filter: `brightness(${Math.max(1 - scrollY / 700, 0.4)})` // 스크롤에 따라 밝기 조절
+                    }}
+                />
                 <div className='background background__noise-effect' />
-                <h1 className='title'>
+                <h1 className='title'
+                    style={{
+                        filter: `brightness(${Math.max(1 - scrollY / 700, 0.4)})` // 스크롤에 따라 밝기 조절
+                    }}
+                >
                     안녕하세요,<br />
                     프론트엔드 개발자 <strong className='title__large-text title__bounce-text'>"박다현"</strong> 입니다.
                 </h1>
