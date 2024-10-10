ab03ca771f778e6c1b5bbec61d0fa12703061b10
박다현 <oing_0ing@naver.com>
2024-10-10 18:12:53 +0900

    241010 - skill section 구현 중

--- src/components/sections/Project.jsx
+++ src/components/sections/Project.jsx
@@ -10,14 +10,14 @@ export default function Project({ projectRef }) {
     return (
         <>
             <section className='project' ref={projectRef}>
-                <h2 className='project__heading'>project</h2>
+                <h2 className='project__heading'>PROJECT</h2>
                 <div className='project__grid'>
                     <article className='project__item'>
                         <img className='project__item-thumbnail' src={personal_project_01} alt="프로젝트 이미지" />
                         <h3 className='project__item-title'>진행중 - 개인 포트폴리오</h3>
                         <a href='https://github.com/o0oiiiiing/o0oiiiiing.github.io.git' target='_blank' className='link'>소스코드 (github)</a>
                         <h4 className='project__item-description'>웹 디자인 진행 중</h4>
-                        <h4 className='project__item-description'>(personal) 개인 포트폴리오 사이트</h4>
+                        <h4 className='project__item-description'>포트폴리오를 한 눈에 보기 쉽게 사이트로 제작하였습니다.</h4>
                     </article>
                     <article className='project__item'>
                         <img className='project__item-thumbnail' src={team_project_03} alt="프로젝트 이미지" />
@@ -27,7 +27,7 @@ export default function Project({ projectRef }) {
                         <a href='https://github.com/ICT-pretzel/Pretzel_Next_admin' target='_blank' className='link'>소스코드 - admin (github)</a>
                         <br />
                         <a href='https://github.com/ICT-pretzel/Pretzel_Next_user' target='_blank' className='link'>소스코드 - user (github)</a>
-                        <h4 className='project__item-description'>(team) 영화 OTT 사이트</h4>
+                        <h4 className='project__item-description'>(team) 영화 OTT 서비스를 제공하는 사이트입니다.</h4>
                     </article>
                     <article className='project__item'>
                         <img className='project__item-thumbnail project__item-thumbnail--light' src={team_project_02} alt="프로젝트 이미지" />
@@ -35,7 +35,7 @@ export default function Project({ projectRef }) {
                         <a href='https://www.figma.com/design/12h0iVmQPgLmmot9PuNr4B/%EC%9B%B9-%EB%94%94%EC%9E%90%EC%9D%B8?node-id=0-1&t=DNbm5H6Nci4PmtrT-1' target='_blank' className='link'>웹 디자인 (figma)</a>
                         <br />
                         <a href='https://github.com/o0oiiiiing/ICT_project02' target='_blank' className='link'>소스코드 (github)</a>
-                        <h4 className='project__item-description'>(team) 여행 정보 사이트</h4>
+                        <h4 className='project__item-description'>(team) 제주도의 여행 정보를 제공하는 사이트입니다.</h4>
                     </article>
                     <article className='project__item'>
                         <img className='project__item-thumbnail' src={team_project_01} alt="프로젝트 이미지" />
@@ -43,7 +43,7 @@ export default function Project({ projectRef }) {
                         <a href='https://www.figma.com/design/12h0iVmQPgLmmot9PuNr4B/%EC%9B%B9-%EB%94%94%EC%9E%90%EC%9D%B8?node-id=0-1&t=DNbm5H6Nci4PmtrT-1' target='_blank' className='link'>웹 디자인 (figma)</a>
                         <br />
                         <a href='https://github.com/o0oiiiiing/ICT_project01' target='_blank' className='link'>소스코드 (github)</a>
-                        <h4 className='project__item-description'>(team) 향수 쇼핑몰 사이트</h4>
+                        <h4 className='project__item-description'>(team) 향수를 주제로 한 쇼핑몰 사이트입니다.</h4>
                     </article>
                 </div>
             </section>
--- src/components/sections/Skill.jsx
+++ src/components/sections/Skill.jsx
@@ -1,13 +1,40 @@
 import React from 'react';
 import '../../styles/sections/skill.css';
+import html from '../../assets/skills/html.png'
+import css from '../../assets/skills/css.png'
+import js from '../../assets/skills/js.png'
+import jsp from '../../assets/skills/jsp.png'
+import react from '../../assets/skills/react.png'
+import nextJs from '../../assets/skills/nextJs.png'
+import emotion from '../../assets/skills/emotion.png'
+import mobX from '../../assets/skills/mobX.png'
 
 export default function Skill({ skillRef }) {
 
     return (
         <>
-            <section className='testtest' ref={skillRef}>
-                <div className='test'></div>
-                <div className='test2'></div>
+            <section className='skill' ref={skillRef}>
+                <h2 className='skill__heading'>skill</h2>
+                <article>
+                    <h3>Web Technologies</h3>
+                    <img className='skill__icon' src={html} alt="HTML" />
+                    <img className='skill__icon' src={css} alt="CSS" />
+                    <img className='skill__icon' src={js} alt="JavaScript" />
+                    <img className='skill__icon' src={jsp} alt="JSP" />
+                </article>
+                <article>
+                    <h3>Frontend Frameworks/Libraries</h3>
+                    <img className='skill__icon' src={react} alt="React" />
+                    <img className='skill__icon' src={nextJs} alt="Next.js" />
+                    <img className='skill__icon' src={emotion} alt="emotion" />
+                    <img className='skill__icon' src={mobX} alt="MobX" />
+                </article>
+                <article>
+                    <h3>Tools</h3>
+                </article>
+                <article>
+                    <h3>Cooperation</h3>
+                </article>
             </section>
         </>
     )
