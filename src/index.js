b3491f491f27c890d41f912b534254c94f5055dd
박다현 <oing_0ing@naver.com>
2024-10-21 01:25:17 +0900

    241021 - 미디어 쿼리 적용

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import App from './App';
 import './index.css';
 import './styles/commons/reset.css';
 import './styles/commons/font.css';
+import './styles/mediaQuery/styles_0-479.css';
+import './styles/mediaQuery/styles_480-767.css';
+import './styles/mediaQuery/styles_768-1023.css';
+import './styles/mediaQuery/styles_1024-1279.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
