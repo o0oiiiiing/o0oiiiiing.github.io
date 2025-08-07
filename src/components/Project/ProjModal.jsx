33c6af4926b17fa2dfcdd3f26cc8cdc2360fa448
o0oiiiiing <oing_0ing@naver.com>
2025-08-07 17:05:25 +0900

    ✨ feat(modal): 모달 열릴 때 body 스크롤 방지 기능 추가

--- src/components/Project/ProjModal.jsx
+++ src/components/Project/ProjModal.jsx
@@ -3,6 +3,16 @@ import React from 'react';
 /* item의 썸네일을 클릭하면 나오는 모달 */
 export default function ProjModal({ projName, onClose }) {
 
+    useEffect(() => {
+        // 모달 열릴 때 body 스크롤 막기
+        document.body.style.overflow = 'hidden';
+
+        return () => {
+            // 모달 닫힐 때 복구
+            document.body.style.overflow = 'auto';
+        };
+    }, []);
+
     return (
         <>
             <article className='proj__modal'>
