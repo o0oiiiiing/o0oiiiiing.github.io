f41ef2ac32093ffb6b4379d56e7d16ec1983c77a
o0oiiiiing <oing_0ing@naver.com>
2025-08-07 17:16:14 +0900

    📝 chore(data): projDetailData 내용 추가

--- src/data/ProjDetailData.jsx
+++ src/data/ProjDetailData.jsx
@@ -6,8 +6,38 @@ export const projDetailData = [
         mainImage: projImages.personal_project_01,
         description: [
             {
-                subtitle: '프로젝트 소개',
+                subtitle: '👩🏻‍💻 프로젝트 소개',
                 content: 'React를 활용하여 포트폴리오를 한 눈에 보기 쉽게 사이트로 제작하였습니다.'
+            }
+        ],
+    },
+    {
+        title: 'pretzel',
+        mainImage: projImages.team_project_03,
+        description: [
+            {
+                subtitle: '👩🏻‍💻 프로젝트 소개',
+                content: '영화 OTT 서비스를 제공하는 사이트입니다.'
+            }
+        ],
+    },
+    {
+        title: '제주여행',
+        mainImage: projImages.team_project_02,
+        description: [
+            {
+                subtitle: '👩🏻‍💻 프로젝트 소개',
+                content: '제주도의 여행 정보를 제공하는 사이트입니다.'
+            }
+        ],
+    },
+    {
+        title: 'forest',
+        mainImage: projImages.team_project_01,
+        description: [
+            {
+                subtitle: '👩🏻‍💻 프로젝트 소개',
+                content: '향수를 주제로 한 쇼핑몰 사이트입니다.'
             }
         ],
     }
