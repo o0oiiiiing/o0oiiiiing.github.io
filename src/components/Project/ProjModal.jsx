93c522bacfa76bf726535e94212937b290ec2d4e
o0oiiiiing <oing_0ing@naver.com>
2025-08-07 17:10:36 +0900

    ✨ feat(modal): 프로젝트 상세 내용 모달 구조 및 콘텐츠 추가

--- src/components/Project/ProjModal.jsx
+++ src/components/Project/ProjModal.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { projDetailData } from '../../data/ProjDetailData';
 
 /* item의 썸네일을 클릭하면 나오는 모달 */
 export default function ProjModal({ projName, onClose }) {
+
+    const proj = projDetailData.find(item => item.title === projName);
 
     useEffect(() => {
         // 모달 열릴 때 body 스크롤 막기
@@ -19,7 +22,8 @@ export default function ProjModal({ projName, onClose }) {
                 <div className='proj__modal-scrim' onClick={onClose}></div>
                 <div className='proj__modal-content'>
                     <span className='proj__modal-close material-symbols-rounded' onClick={onClose}>close</span>
-                    <h1 className='proj__modal-heading'>{projName}</h1>
+                    <h1 className='proj__modal-heading'>{proj.title}</h1>
+                    <img className='proj__modal-image' src={proj.mainImage} alt={proj.title} />
                 </div>
             </article>
         </>
--- src/data/ProjDetailData.jsx
+++ src/data/ProjDetailData.jsx
@@ -1,1 +1,14 @@
-export const projDetailData = [];
+import { projImages } from '../components/Project/ProjImages';
+
+export const projDetailData = [
+    {
+        title: '개인 포트폴리오',
+        mainImage: projImages.personal_project_01,
+        description: [
+            {
+                subtitle: '프로젝트 소개',
+                content: 'React를 활용하여 포트폴리오를 한 눈에 보기 쉽게 사이트로 제작하였습니다.'
+            }
+        ],
+    }
+];
